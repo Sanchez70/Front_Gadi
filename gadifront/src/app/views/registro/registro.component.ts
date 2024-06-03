5f97b8d3709234c3977af4bdfa0868b80178b0be
Santana <carsantana54@gmail.com>
2024-06-03 14:16:39 -0500

    registro

--- gadifront/src/app/views/registro/registro.component.ts
+++ gadifront/src/app/views/registro/registro.component.ts
@@ -1,12 +1,26 @@
 import { Component } from "@angular/core";
+import {FloatLabelType, MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, FormGroup, FormsModule, FormControl } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 @Component({
     selector: 'app-registro',
-    templateUrl: './registro.component.html',
-    styleUrl: './registro.component.css'
+    templateUrl: './registro1.component.html',
+    styleUrl: './registro.component.css',
+    standalone: true,
+    imports: [
+        FormsModule,
+        MatRadioModule,
+      ],
 })
 
 export class RegistroComponent{
+    public registro1!: FormGroup; 
+    public registro2!: FormGroup;
 
+    floatLabelControl = new FormControl('auto' as FloatLabelType);
+
+    onSubmit(){}
 }
+
