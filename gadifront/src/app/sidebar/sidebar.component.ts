da7de064b37fa3e3880a17d28e4dcf95d87a33b9
Alewho777 <4lej4ndr0flor3s69@gmail.com>
2024-06-07 14:17:50 -0500

    fix tabla, login, sidebar

--- gadifront/src/app/sidebar/sidebar.component.ts
+++ gadifront/src/app/sidebar/sidebar.component.ts
@@ -3,26 +3,24 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { AuthService } from '../auth.service';
-import { style } from '@angular/animations';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  @ViewChild('sidenav')
-  sidenav!: MatSidenav;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
   isExpanded = false;
- explan :string ='Abrir';
+  explan: string = 'Abrir';
 
   constructor(private authService: AuthService, private router: Router) { }
 
   toggleSidenav() {
-    this.sidenav.toggle();
-   
     this.isExpanded = !this.isExpanded;
-
+    this.sidenav.toggle();
   }
+
   cerrar(): void {
     Swal.fire({
       title: '¿Estás seguro?',
@@ -39,20 +37,14 @@ export class SidebarComponent {
         this.authService.clearLocalStorage();
       }
     });
-
   }
 
-  navbar() :void{
-
-    if(this.explan ==='Abrir'){
-      this.explan='Cerrar';
-      this.authService.navbar();
-      this.toggleSidenav();
-    }else if(this.explan ==='Cerrar'){
-      this.explan ='Abrir';
-       this.toggleSidenav();
-       this.authService.navbar();
+  navbar(): void {
+    if (this.explan === 'Abrir') {
+      this.explan = 'Cerrar';
+    } else if (this.explan === 'Cerrar') {
+      this.explan = 'Abrir';
     }
-
+    this.toggleSidenav();
   }
 }
