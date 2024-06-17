3f24d27fe4c19a641bdd8d1629944d125d83812f
Sanchez70 <kevinalexsanchez2051@gmail.com>
2024-06-17 13:14:13 -0500

    Director70%

--- gadifront/src/app/views/matriz-propuesta/matriz-propuesta.component.ts
+++ gadifront/src/app/views/matriz-propuesta/matriz-propuesta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AsignaturaService } from '../../Services/asignaturaService/asignatura.service';
 import { CicloService } from '../../Services/cicloService/ciclo.service';
@@ -20,6 +20,13 @@ import { GradoOcupacionalService } from '../../Services/grado/grado-ocupacional.
 import { tipo_actividad } from '../../Services/tipo_actividadService/tipo_actividad';
 import { tipo_actividadService } from '../../Services/tipo_actividadService/tipo_actividad.service';
 import { PeriodoService } from '../../Services/periodoService/periodo.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { catchError, forkJoin, of, switchMap, tap } from 'rxjs';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { GradoOcupacional } from '../grado-ocupacional/grado-ocupacional';
+import { TituloProfecional } from '../titulo-profesional/titulo-profecional';
+import { TipoContrato } from '../tipo-contrato/tipo-contrato';
 interface PersonaExtendida extends Persona {
   nombre_contrato?: string;
   nombre_titulo?: string;
@@ -30,67 +37,84 @@ interface PersonaExtendida extends Persona {
   templateUrl: './matriz-propuesta.component.html',
   styleUrl: './matriz-propuesta.component.css'
 })
-export class MatrizPropuestaComponent implements OnInit{
-asignaturas: any[] = [];
-actividades: any[] = [];
-jornadas: any[] = [];
-periodos: any[] = [];
-ciclos: any[] = [];
-carreras: any[] = [];
-distributivos: any[] = [];
-distributivoFiltrado: any[] = [];
-distributivoAsignaturas: any[] = [];
-distributivoAsignaturasFiltrado: any[] = [];
-distributivoActividades: any[] = [];
-distributivoActividadesFiltrado: any[] = [];
-asignaturasFiltradas: any[] = [];
-actividadesFiltradas: any[] = [];
-public Tipos: tipo_actividad[] = [];
-cedula: string = '';
-periodo: number = 0;
-periodoNombre: string = '';
-personaEncontrada : Persona = new Persona();
-personaExtendida : PersonaExtendida = new Persona();
-gradoOcupacional : Grado_ocupacional = new Grado_ocupacional();
-tipo_contrato: Tipo_contrato = new Tipo_contrato();
-titulo: Titulo_profesional = new Titulo_profesional();
+export class MatrizPropuestaComponent implements OnInit {
+  displayedColumns: string[] = ['cedula', 'nombre', 'apellido', 'telefono', 'direccion', 'correo', 'edad', 'fecha_vinculacion', 'contrato', 'titulo', 'grado'];
+  dataSource!: MatTableDataSource<Persona>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+    grados: { [key: number]: GradoOcupacional } = {};
+    titulos: { [key: number]: TituloProfecional } = {};
+    contratos: { [key: number]: TipoContrato } = {};
+  personas: Persona[] = [];
+  asignaturas: any[] = [];
+  actividades: any[] = [];
+  jornadas: any[] = [];
+  periodos: any[] = [];
+  ciclos: any[] = [];
+  carreras: any[] = [];
+  distributivos: any[] = [];
+  distributivoFiltrado: any[] = [];
+  distributivoAsignaturas: any[] = [];
+  distributivoAsignaturasFiltrado: any[] = [];
+  distributivoActividades: any[] = [];
+  distributivoActividadesFiltrado: any[] = [];
+  asignaturasFiltradas: any[] = [];
+  actividadesFiltradas: any[] = [];
+  public Tipos: tipo_actividad[] = [];
+  cedula: string = '';
+  periodo: number = 0;
+  color = '#1E90FF';
+  periodoNombre: string = '';
+  personaEncontrada: Persona = new Persona();
+  personaExtendida: PersonaExtendida = new Persona();
+  gradoOcupacional: Grado_ocupacional = new Grado_ocupacional();
+  tipo_contrato: Tipo_contrato = new Tipo_contrato();
+  titulo: Titulo_profesional = new Titulo_profesional();
+  currentExplan: string = '';
   constructor(
-    private asignaturaService: AsignaturaService, 
+    private asignaturaService: AsignaturaService,
     private personaService: PersonaService,
     private cicloService: CicloService,
-    private carreraService: CarreraService, 
-    private jornadaService: JornadaService, 
-    private distributivoAsignaturaService: DistributivoAsignaturaService, 
-    private distributivoService: DistributivoService, 
-    private distributivoActividadService: DistributivoActividadService, 
-    private actividadService: ActividadService, 
-    private tipo_contratoService: TipoContratoService, 
-    private tituloService: TituloProfesionalService, 
+    private carreraService: CarreraService,
+    private jornadaService: JornadaService,
+    private distributivoAsignaturaService: DistributivoAsignaturaService,
+    private distributivoService: DistributivoService,
+    private distributivoActividadService: DistributivoActividadService,
+    private actividadService: ActividadService,
+    private tipo_contratoService: TipoContratoService,
+    private tituloService: TituloProfesionalService,
     private gradoService: GradoOcupacionalService,
     private tipo_actividadService: tipo_actividadService,
     private periodoService: PeriodoService,
-    private authService: AuthService, 
+    private authService: AuthService,
     private router: Router,
-    private activatedRoute: ActivatedRoute){
+    private activatedRoute: ActivatedRoute) {
 
   }
-  ngOnInit(): void{
+  ngOnInit(): void {
+    this.authService.explan$.subscribe(explan => {
+      this.currentExplan = explan;
+    });
     this.cargarCarreras();
     this.cargarCiclos();
     this.cargarAsignaturas();
     this.cargarActividades();
     this.cargarJornadas();
     this.cargarPeriodos();
+    this.personaService.getPersonas().subscribe(data => {
+      this.personas = data;
+      this.loadAdditionalDataForPersonas();
+  });
   }
 
-  buscarPersona(): void{
-    this.personaService.getPersonaByCedula(this.cedula).subscribe(data =>{
+  buscarPersona(): void {
+    this.personaService.getPersonaByCedula(this.cedula).subscribe(data => {
       this.personaEncontrada = data;
-      console.log('id_persona',this.personaEncontrada.id_persona)
+      console.log('id_persona', this.personaEncontrada.id_persona)
       this.loadPersonaData(this.personaEncontrada.id_persona);
       this.buscarDistributivo(this.personaEncontrada.id_persona);
     });
-    
+
   }
 
   loadPersonaData(personaId: number): void {
@@ -99,41 +123,41 @@ titulo: Titulo_profesional = new Titulo_profesional();
       this.tipo_contratoService.getcontratobyId(data.id_tipo_contrato).subscribe(contratos => {
         this.tipo_contrato = contratos;
         this.personaExtendida.nombre_contrato = this.tipo_contrato.nombre_contrato;
-        
+
       });
       this.tituloService.getTitulobyId(data.id_titulo_profesional).subscribe(titulos => {
         this.titulo = titulos;
         this.personaExtendida.nombre_titulo = this.titulo.nombre_titulo;
-       
+
       });
       this.gradoService.getGradobyId(data.id_grado_ocp).subscribe(grados => {
         this.gradoOcupacional = grados;
         this.personaExtendida.nombre_grado_ocp = this.gradoOcupacional.nombre_grado_ocp;
-        
+
       });
     });
   }
 
-  
- cargarAsignaturas(): void{
-  this.asignaturaService.getAsignatura().subscribe(data =>{
-    this.asignaturas = data;
-   });
- }
 
-  cargarActividades(): void{
-    this.actividadService.getActividad().subscribe(data =>{
+  cargarAsignaturas(): void {
+    this.asignaturaService.getAsignatura().subscribe(data => {
+      this.asignaturas = data;
+    });
+  }
+
+  cargarActividades(): void {
+    this.actividadService.getActividad().subscribe(data => {
       this.actividades = data;
-      });
-    }
+    });
+  }
 
-  cargarCiclos(): void{
+  cargarCiclos(): void {
     this.cicloService.getCiclo().subscribe(data => {
       this.ciclos = data;
     });
   }
 
-  cargarJornadas(): void{
+  cargarJornadas(): void {
     this.jornadaService.getJornada().subscribe(data => {
       this.jornadas = data;
     });
@@ -157,78 +181,78 @@ titulo: Titulo_profesional = new Titulo_profesional();
     });
   }
 
-  buscarDistributivo(idPersona:number): void{
-    this.distributivoService.getDistributivo().subscribe(data =>{
+  buscarDistributivo(idPersona: number): void {
+    this.distributivoService.getDistributivo().subscribe(data => {
       this.distributivos = data;
       this.distributivoFiltrado = this.distributivos.filter(
         distributivo => distributivo.id_persona === idPersona);
-        console.log('distributivo encontrado',this.distributivoFiltrado);
-        this.periodo = this.distributivoFiltrado[0].id_periodo;
-        console.log('periodo', this.periodo);
-        
-        this.obtenerNombrePeriodo(this.periodo);
-        this.buscarAsignatura(this.distributivoFiltrado[0].id_distributivo);
-        this.buscarActividad(this.distributivoFiltrado[0].id_distributivo)
+      console.log('distributivo encontrado', this.distributivoFiltrado);
+      this.periodo = this.distributivoFiltrado[0].id_periodo;
+      console.log('periodo', this.periodo);
+
+      this.obtenerNombrePeriodo(this.periodo);
+      this.buscarAsignatura(this.distributivoFiltrado[0].id_distributivo);
+      this.buscarActividad(this.distributivoFiltrado[0].id_distributivo)
     });
-    
+
     ;
   }
 
-  buscarAsignatura(idDistributivo: number): void{
-    this.distributivoAsignaturaService.getDistributivoAsignatura().subscribe(data =>{
+  buscarAsignatura(idDistributivo: number): void {
+    this.distributivoAsignaturaService.getDistributivoAsignatura().subscribe(data => {
       this.distributivoAsignaturas = data;
       this.distributivoAsignaturasFiltrado = this.distributivoAsignaturas.filter(
         distributivoAsignatura => distributivoAsignatura.id_distributivo === idDistributivo);
-        console.log('distributivo asignatura',this.distributivoAsignaturasFiltrado);
-        
-        //asignar paralelos a cada materia
-        const asignaturasParalelosMap = this.distributivoAsignaturasFiltrado.reduce((map, item) => {
-          map[item.id_asignatura] = item.paralelo;
-          return map;
+      console.log('distributivo asignatura', this.distributivoAsignaturasFiltrado);
+
+      //asignar paralelos a cada materia
+      const asignaturasParalelosMap = this.distributivoAsignaturasFiltrado.reduce((map, item) => {
+        map[item.id_asignatura] = item.paralelo;
+        return map;
       }, {});
 
       const asignaturasJornadaMap = this.distributivoAsignaturasFiltrado.reduce((map, item) => {
         map[item.id_asignatura] = item.id_jornada;
         return map;
-    }, {});
+      }, {});
 
-        const idAsignaturas = this.distributivoAsignaturasFiltrado.map(distributivoAsignaturaEncontrados => distributivoAsignaturaEncontrados.id_asignatura);
-        console.log('id_asignaturas', idAsignaturas);
+      const idAsignaturas = this.distributivoAsignaturasFiltrado.map(distributivoAsignaturaEncontrados => distributivoAsignaturaEncontrados.id_asignatura);
+      console.log('id_asignaturas', idAsignaturas);
 
-        this.asignaturasFiltradas = this.asignaturas.filter(asignatura =>
-          idAsignaturas.includes(asignatura.id_asignatura)
-        ).map(asignatura => ({
-            ...asignatura,
-            paralelo: asignaturasParalelosMap[asignatura.id_asignatura],
-            jornada: asignaturasJornadaMap[asignatura.id_asignatura]
-        }));
+      this.asignaturasFiltradas = this.asignaturas.filter(asignatura =>
+        idAsignaturas.includes(asignatura.id_asignatura)
+      ).map(asignatura => ({
+        ...asignatura,
+        paralelo: asignaturasParalelosMap[asignatura.id_asignatura],
+        jornada: asignaturasJornadaMap[asignatura.id_asignatura]
+      }));
 
-        console.log('detalle asignaturas cargadas', this.asignaturasFiltradas);
-    });  
+      console.log('detalle asignaturas cargadas', this.asignaturasFiltradas);
+    });
   }
 
-  
-  buscarActividad(idDistributivo: number): void{
-    this.distributivoActividadService.getDistributivoActividad().subscribe(data =>{
+
+  buscarActividad(idDistributivo: number): void {
+    this.distributivoActividadService.getDistributivoActividad().subscribe(data => {
       this.distributivoActividades = data;
       this.distributivoActividadesFiltrado = this.distributivoActividades.filter(
-        distributivoActividad => 
+        distributivoActividad =>
           distributivoActividad.id_distributivo === idDistributivo);
-        console.log('distributivo actividad',this.distributivoActividadesFiltrado)
+      console.log('distributivo actividad', this.distributivoActividadesFiltrado)
 
-        const idActividades = this.distributivoActividadesFiltrado.map(distributivoActividadEncontrados => distributivoActividadEncontrados.id_actividad);
-        console.log('id_actividad', idActividades);
+      const idActividades = this.distributivoActividadesFiltrado.map(distributivoActividadEncontrados => distributivoActividadEncontrados.id_actividad);
+      console.log('id_actividad', idActividades);
 
-        this.actividadesFiltradas = this.actividades.filter(actividad =>
-          idActividades.includes(actividad.id_actividad));
-          console.log('detalle cargadas',this.actividadesFiltradas);
+      this.actividadesFiltradas = this.actividades.filter(actividad =>
+        idActividades.includes(actividad.id_actividad));
+      console.log('detalle cargadas', this.actividadesFiltradas);
     });
-   
+
   }
 
- 
 
-  obtenerNombreCiclo(id_ciclo:number):void{
+
+  obtenerNombreCiclo(id_ciclo: number): void {
     const ciclo = this.ciclos.find(ciclo => ciclo.id_ciclo === id_ciclo);
     return ciclo ? ciclo.nombre_ciclo : '';
   }
@@ -254,10 +278,59 @@ titulo: Titulo_profesional = new Titulo_profesional();
     return tipoActividad ? tipoActividad.nom_tip_actividad : '';
   }
 
-  onChangeBuscar(event: any): void{
+  onChangeBuscar(event: any): void {
     this.cedula = event.target.value;
-    console.log('cedula ingresada',this.cedula)
+    console.log('cedula ingresada', this.cedula)
   }
+  loadAdditionalDataForPersonas(): void {
+    const requests = this.personas.map(persona =>
+        this.personaService.getPeriodoById(persona.id_persona).pipe(
+            switchMap(() => forkJoin([
+                this.personaService.getGradoById(persona.id_grado_ocp ?? 0).pipe(
+                    tap(grado => {
+                        this.grados[persona.id_persona] = grado ?? { id_grado_ocp: 0, nombre_grado_ocp: 'No asignado' };
+                    }),
+                    catchError(() => {
+                        this.grados[persona.id_persona] = { id_grado_ocp: 0, nombre_grado_ocp: 'No asignado' } as GradoOcupacional;
+                        return of(null);
+                    })
+                ),
+                this.personaService.getTituloById(persona.id_titulo_profesional ?? 0).pipe(
+                    tap(titulo => {
+                        this.titulos[persona.id_persona] = titulo ?? { id_titulo_profesional: 0, nombre_titulo: 'No asignado' };
+                    }),
+                    catchError(() => {
+                        this.titulos[persona.id_persona] = { id_titulo_profesional: 0, nombre_titulo: 'No asignado' } as unknown as TituloProfecional;
+                        return of(null);
+                    })
+                ),
+                this.personaService.getContratoById(persona.id_tipo_contrato ?? 0).pipe(
+                    tap(contrato => {
+                        this.contratos[persona.id_persona] = contrato ?? { id_tipo_contrato: 0, nombre_contrato: 'No asignado' };
+                    }),
+                    catchError(() => {
+                        this.contratos[persona.id_persona] = { id_tipo_contrato: 0, nombre_contrato: 'No asignado' } as unknown as TipoContrato;
+                        return of(null);
+                    })
+                )
+            ]))
+        )
+    );
 
+    forkJoin(requests).subscribe(() => {
+        this.dataSource = new MatTableDataSource(this.personas);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+    });
+}
+
+applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+
+    if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+    }
+}
 
 }
