549f8916d2086c25fa4dea6e4b0f564b252ee990
Edisson <fabianleon184@gmail.com>
2024-06-20 11:07:34 -0500

    Encriptacion Contra

--- gadifront/src/app/views/login/login.component.ts
+++ gadifront/src/app/views/login/login.component.ts
@@ -9,6 +9,8 @@ import { SrolService } from '../../Services/rol/srol.service';
 import { UsuarioRolService } from '../../Services/UsuarioRol/usuario-rol.service';
 import { Rol } from '../../Services/rol/rol';
 import { UsuarioRol } from '../../Services/UsuarioRol/usuarioRol';
+import * as bcrypt from 'bcryptjs';
+
 const Toast = Swal.mixin({
   toast: true,
   position: "bottom-end",
@@ -41,48 +43,98 @@ export class LoginComponent {
     this.validar();
   }
 
+
   validar(): void {
     const usuariol = this.searchForm.value.usuario;
     const contraneusu = this.searchForm.value.contraneusu;
-  
+
     if (usuariol === '') {
       Toast.fire({
         icon: "error",
         title: "Campo Usuario Vacio",
         footer: "Por favor, verifique si ha completado todo lo necesario"
       });
-
     } else if (contraneusu === '') {
       Toast.fire({
         icon: "error",
         title: "Campo Contraseña Vacio",
         footer: "Por favor, verifique si ha completado todo lo necesario"
       });
-
     } else {
       this.loginService.getUsuario().subscribe(
         (result) => {
           if (Array.isArray(result) && result.length > 0) {
             const usuarioEncontrados = result as Usuario[];
-            const usuarioEncontrado = usuarioEncontrados.find(usuario => usuario.contrasena === contraneusu && usuario.usuario === usuariol);
-            console.log('Usuario encontrado:', usuarioEncontrado);
-            console.log('ID de carrera del usuario encontrado:', usuarioEncontrado?.carrera?.id_carrera);
+            const usuarioEncontrado = usuarioEncontrados.find(usuario => usuario.usuario === usuariol);
             if (usuarioEncontrado) {
-              this.cargarRol(usuarioEncontrado.id_usuario, usuariol);
-              this.authService.id_carrera = usuarioEncontrado.carrera?.id_carrera;
-              console.log(this.authService.id_carrera)
-
+              // Encriptar la contraseña ingresada por el usuario
+              bcrypt.compare(contraneusu, usuarioEncontrado.contrasena, (err, res) => {
+                if (err) {
+                  console.error('Error al comparar contraseñas:', err);
+                  return;
+                }
+                if (res) {
+                  this.cargarRol(usuarioEncontrado.id_usuario, usuariol);
+                  this.authService.id_carrera = usuarioEncontrado.carrera?.id_carrera;
+                } else {
+                  Swal.fire('Usuario o contraseña incorrecta', 'Intente nuevamente', 'error');
+                }
+              });
             } else {
-              Swal.fire('Usuario o contraseña erronea', 'Intente nuevamente', 'error');
-
+              Swal.fire('Usuario no encontrado', 'Intente nuevamente', 'error');
             }
           }
-
-        }, (error) => {
-          Swal.fire('Campos Vacios', 'Intente nuevamente', 'error');
-        });
+        },
+        (error) => {
+          // Manejo de error para la solicitud de usuario
+          Swal.fire('Error al obtener los datos del usuario', 'Intente nuevamente', 'error');
+        }
+      );
     }
   }
+
+  // validar(): void {
+  //   const usuariol = this.searchForm.value.usuario;
+  //   const contraneusu = this.searchForm.value.contraneusu;
+
+  //   if (usuariol === '') {
+  //     Toast.fire({
+  //       icon: "error",
+  //       title: "Campo Usuario Vacio",
+  //       footer: "Por favor, verifique si ha completado todo lo necesario"
+  //     });
+
+  //   } else if (contraneusu === '') {
+  //     Toast.fire({
+  //       icon: "error",
+  //       title: "Campo Contraseña Vacio",
+  //       footer: "Por favor, verifique si ha completado todo lo necesario"
+  //     });
+
+  //   } else {
+  //     this.loginService.getUsuario().subscribe(
+  //       (result) => {
+  //         if (Array.isArray(result) && result.length > 0) {
+  //           const usuarioEncontrados = result as Usuario[];
+  //           const usuarioEncontrado = usuarioEncontrados.find(usuario => usuario.contrasena === contraneusu && usuario.usuario === usuariol);
+  //           console.log('Usuario encontrado:', usuarioEncontrado);
+  //           console.log('ID de carrera del usuario encontrado:', usuarioEncontrado?.carrera?.id_carrera);
+  //           if (usuarioEncontrado) {
+  //             this.cargarRol(usuarioEncontrado.id_usuario, usuariol);
+  //             this.authService.id_carrera = usuarioEncontrado.carrera?.id_carrera;
+  //             console.log(this.authService.id_carrera)
+
+  //           } else {
+  //             Swal.fire('Usuario o contraseña erronea', 'Intente nuevamente', 'error');
+
+  //           }
+  //         }
+
+  //       }, (error) => {
+  //         Swal.fire('Campos Vacios', 'Intente nuevamente', 'error');
+  //       });
+  //   }
+  // }
 
   selectedRole: string | undefined;
   cargarRol(id_rol: any, usuario: any): void {
