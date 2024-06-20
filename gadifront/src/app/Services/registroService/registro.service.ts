3547f7393ae40c2a5fdccb310c47a815da17f7ff
Santana <carsantana54@gmail.com>
2024-06-20 11:29:39 -0500

    Registro 98%

--- gadifront/src/app/Services/registroService/registro.service.ts
+++ gadifront/src/app/Services/registroService/registro.service.ts
@@ -4,41 +4,37 @@ import { Observable, throwError } from 'rxjs';
 import { Persona } from '../../Services/docenteService/persona';
 import { Usuario } from '../../Services/loginService/usuario';
 import { Tipo_contrato } from '../../Services/tipo_contrato/tipo_contrato';
-import { Grado_ocupacional } from '../../Services/grado/grado_ocupacional';
 import { Titulo_profesional } from '../../Services/titulo/titulo_profesional';
+import { UsuarioRol } from '../UsuarioRol/usuarioRol';
+import { appConfig } from '../../environment/appConfig';
 
 @Injectable({
     providedIn: 'root'
   })
 
 export class RegistroService{
-    private apiUrlPer = 'http://localhost:8081/api/persona';
-    private apiUrlUser = 'http://localhost:8081/api/usuario';
-    private apiUrlContr = 'http://localhost:8081/api/tipo_contrato';
-    private apiUrlTitulo = 'http://localhost:8081/api/titulo_profesional';
-    private apiUrlGrado = 'http://localhost:8081/api/grado_ocupacional';
+    private urlBase =  appConfig.baseUrl;
     private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
 
     constructor(private http: HttpClient) { }
 
-    createPer(persona:Persona):Observable<Persona>{
-        return this.http.post<Persona>(this.apiUrlPer, persona, {headers: this.httpHeaders})
-    }
+    createPer(persona: Persona): Observable<Persona> {
+        return this.http.post<Persona>(`${this.urlBase}/persona`, persona, { headers: this.httpHeaders})
+      }
 
     createUser(user:Usuario):Observable<Usuario>{
-        return this.http.post<Usuario>(this.apiUrlUser, user, {headers: this.httpHeaders})
+        return this.http.post<Usuario>(`${this.urlBase}/usuario`, user, {headers: this.httpHeaders})
     }
 
     createContrato(contrato:Tipo_contrato):Observable<Tipo_contrato>{
-        return this.http.post<Tipo_contrato>(this.apiUrlContr, contrato, {headers: this.httpHeaders})
+        return this.http.post<Tipo_contrato>(`${this.urlBase}/tipo_contrato`, contrato, {headers: this.httpHeaders})
     }
 
     createTitulo(titulo:Titulo_profesional):Observable<Titulo_profesional>{
-        return this.http.post<Titulo_profesional>(this.apiUrlTitulo, titulo, {headers: this.httpHeaders})
+        return this.http.post<Titulo_profesional>(`${this.urlBase}/titulo_profesional`, titulo, {headers: this.httpHeaders})
     }
 
-    createGrado(grado:Grado_ocupacional):Observable<Grado_ocupacional>{
-        return this.http.post<Grado_ocupacional>(this.apiUrlGrado, grado, {headers: this.httpHeaders})
-    }
-
+    create(usuarioRol: UsuarioRol): Observable<UsuarioRol> {
+        return this.http.post<UsuarioRol>(`${this.urlBase}/usuarioRol`, usuarioRol, { headers: this.httpHeaders })
+      }
 }
